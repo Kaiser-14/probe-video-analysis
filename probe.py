6ad1b5a1c34dce66c9920077eb46b1996132f81e
Kaiser-14 <alberdrp@gmail.com>
2021-03-29 09:50:22 +0200

    Included new data to export and divided sending metrics in Kafka

--- probe.py
+++ probe.py
@@ -8,6 +8,8 @@ import sys
 import kafka
 from kafka import KafkaProducer
 import requests
+from uuid import getnode
+import datetime
 
 parser = argparse.ArgumentParser(description='Multimedia probe analysis.')
 parser.add_argument(
@@ -40,9 +42,11 @@ args = parser.parse_args()
 if args.kafka:
 	try:
 		producer = KafkaProducer(bootstrap_servers=args.kafka[0] + ':' + args.kafka[1])
+		# TODO: Remove exit
 		exit(0)
 	except kafka.errors.NoBrokersAvailable as e:
 		print('There are no Kafka brokers available in ' + args.kafka[0] + ':' + args.kafka[1])
+		# TODO: Remove exit
 		exit(0)
 
 if args.rest:
@@ -52,6 +56,8 @@ if args.rest:
 	except requests.exceptions.ConnectionError:
 		print('Probe could not reach Rest API server')
 		exit(0)
+
+mac_address = ':'.join(re.findall('..', '%012x' % getnode()))
 
 while True:
 	# Convert stream to raw, saving the output locally
@@ -103,29 +109,50 @@ while True:
 	df = df.astype(float) 
 
 	# Manipulate data
-	# print(df['Blockiness'].iloc[-60:].mean())
 	blockiness = df['Blockiness'].iloc[-60:].mean()
-
-	# print(df['Blockloss'].iloc[-60:].mean())
-	blockloss = df['Blockloss'].iloc[-60:].mean()
-
-	# print(df['Blur'].iloc[-60:].mean())
+	spatial_activity = df['SA'].iloc[-60:].mean()
+	block_loss = df['Blockloss'].iloc[-60:].mean()
 	blur = df['Blur'].iloc[-60:].mean()
+	temporal_activity = df['TA'].iloc[-60:].mean()
 
-	qi = (blur * blockiness) / blockloss
+	metrics = {
+		'uuid': mac_address,
+		'value': {
+			'blockiness': blockiness,
+			'spatial_activity': spatial_activity,
+			'block_loss': block_loss,
+			'blur': blur,
+			'temporal_activity': temporal_activity
+		},
+		'timestamp': datetime.datetime.now().timestamp()
+	}
+
+	# qi = (blur * blockiness) / block_loss
 
 	# Print information locally
 	if not args.kafka and not args.rest:
-		print(qi)
+		print('Blockiness: {}'.format(blockiness))
+		print('Spatial Activity: {}'.format(spatial_activity))
+		print('Block Loss: {}'.format(block_loss))
+		print('Blur: {}'.format(blur))
+		print('Temporal Activity: {}'.format(temporal_activity))
 
 	# Send information via Kafka bus
 	if args.kafka:
-		# TODO: Send corresponding metric
-		producer.send(args.topic, qi)
+		# Every metric condensed into one message
+		if len(args.topic) == 1:
+			producer.send(args.topic, metrics)
+		# Publish metrics individually
+		else:
+			producer.send(args.topic[0], blockiness)
+			producer.send(args.topic[0], spatial_activity)
+			producer.send(args.topic[0], block_loss)
+			producer.send(args.topic[0], blur)
+			producer.send(args.topic[0], temporal_activity)
 
 	# Send information via Rest API
 	if args.rest:
-		req = requests.put(args.rest, qi)
+		req = requests.put(args.rest, metrics)
 
 	# Handle process
 	input("Press Enter to continue...")
